697415bd320acabfb41bd2a8fe1eaba73e898fb9
Unknown <thummel42@gmail.com>
2019-01-02 13:34:19 -0800

    Added OS env variables support

--- src/video_main.py
+++ src/video_main.py
@@ -2,17 +2,18 @@
 
 import cv2
 import sys
+import os
 
 #determine what the parameters are for our vision system process
 #get the image size and frame rate, and weather or not to use mjpeg or x264 encoder
 #program to be extended later
 
-vidfile = "/dev/video0"
-if len(sys.argv) > 1:
-    vidfile = sys.argv[1]
 
+vidfile = os.environ.get("TANK_VIDEO_PATH","/dev/video0")
+raw_height = os.environ.get("TANK_VIDEO_HEIGHT", 480)
+raw_width = os.environ.get("TANK_VIDEO_WIDTH", 640)
+framerate = os.environ.get("TANK_VIDEO_FPS", 15)
 
-framerate = 15.0
 
 
 cap = cv2.VideoCapture(vidfile)
